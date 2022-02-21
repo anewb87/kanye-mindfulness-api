81e95067caacf9ae8d700e5efda33dd1b59442c8
Alexis Newby <alexis.newby@gmail.com>
2022-02-21 16:27:46 -0700

    Update data

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
-app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 4020);
 app.locals.title = 'Kanye Mindfulness App';
 
 app.locals.user = {
@@ -12,14 +17,14 @@ app.locals.user = {
     journal: [
     {id: 1, date: '02/21/21', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam urna a sollicitudin porttitor. Sed bibendum arcu metus, sit amet rhoncus ante pulvinar et. Suspendisse sit amet ex aliquam turpis dapibus luctus id quis sapien. Ut vestibulum lorem ut pulvinar scelerisque. Maecenas et nibh vel quam consequat blandit. Nam sem velit, pretium eu velit et, rhoncus porttitor eros. Curabitur auctor eros nisi, a tempor massa pulvinar vel. Donec faucibus nisi et lorem tristique, nec sodales tortor cursus. Praesent tempor ultrices erat, id malesuada arcu tempus a. Vivamus dapibus, nisi tempus varius porttitor, ipsum tellus convallis metus, et bibendum lectus metus id mi. Donec ullamcorper non augue eget consequat.'}, 
     {id: 2, date: '03/07/2003', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam urna a sollicitudin porttitor. Sed bibendum arcu metus, sit amet rhoncus ante pulvinar et. Suspendisse sit amet ex aliquam turpis dapibus luctus id quis sapien. Ut vestibulum lorem ut pulvinar scelerisque. Maecenas et nibh vel quam consequat blandit. Nam sem velit, pretium eu velit et, rhoncus porttitor eros. Curabitur auctor eros nisi, a tempor massa pulvinar vel. Donec faucibus nisi et lorem tristique, nec sodales tortor cursus. Praesent tempor ultrices erat, id malesuada arcu tempus a. Vivamus dapibus, nisi tempus varius porttitor, ipsum tellus convallis metus, et bibendum lectus metus id mi. Donec ullamcorper non augue eget consequat'},
-    {id: 3, date: '01/23/2018', body: "I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. So when you get something that has the name Kanye West on it, it’s supposed to be pushing the furthest possibilities. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus."}
-    {id: 4, date: '09/20/1999', body: `I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. So when you get something that has the name Kanye West on it, it’s supposed to be pushing the furthest possibilities. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus.`}
+      { id: 3, date: '01/23/2018', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquam urna a sollicitudin porttitor. Sed bibendum arcu metus, sit amet rhoncus ante pulvinar et. Suspendisse sit amet ex aliquam turpis dapibus luctus id quis sapien. Ut vestibulum lorem ut pulvinar scelerisque. Maecenas et nibh vel quam consequat blandit. Nam sem velit, pretium eu velit et, rhoncus porttitor eros. Curabitur auctor eros nisi, a tempor massa pulvinar vel. Donec faucibus nisi et lorem tristique, nec sodales tortor cursus. Praesent tempor ultrices erat, id malesuada arcu tempus a. Vivamus dapibus, nisi tempus varius porttitor, ipsum tellus convallis metus, et bibendum lectus metus id mi. Donec ullamcorper non augue eget consequat.'},
+      { id: 4, date: '09/20/1999', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Maecenas aliquam urna a sollicitudin porttitor.Sed bibendum arcu metus, sit amet rhoncus ante pulvinar et.Suspendisse sit amet ex aliquam turpis dapibus luctus id quis sapien.Ut vestibulum lorem ut pulvinar scelerisque.Maecenas et nibh vel quam consequat blandit.Nam sem velit, pretium eu velit et, rhoncus porttitor eros.Curabitur auctor eros nisi, a tempor massa pulvinar vel.Donec faucibus nisi et lorem tristique, nec sodales tortor cursus.Praesent tempor ultrices erat, id malesuada arcu tempus a.Vivamus dapibus, nisi tempus varius porttitor, ipsum tellus convallis metus, et bibendum lectus metus id mi.Donec ullamcorper non augue eget consequat.'}
     ],
     moods: [
-        {id: 1, date: '04/18/2018', mood: '😑'},
-        {id: 2, date: '07/28/2012', mood: '😺'},
-        {id: 3, date: '08/05/2008', mood: '😭'},
-        {id: 4, date: '05/12/2013', mood: '😁'}
+        {id: 1, date: '04/18/2018', mood: 1},
+        {id: 2, date: '07/28/2012', mood: 2},
+        {id: 3, date: '08/05/2008', mood: 3},
+        {id: 4, date: '05/12/2013', mood: 4}
 
     ],
     sleepData: [],
